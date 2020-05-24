3298e64186ec945b81b09fc17c89a540524a30b4
margaret mwangi <margaret13mwangi@gmail.com>
2020-05-25 01:49:37 +0300

    added BASE_url

--- Cam/views.py
+++ Cam/views.py
@@ -1,7 +1,19 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
-# Create your views here.
+BASE_KRAIGSLIST_URL = 'https://kenya.craigslist.org/search/hhh?query={}'
+
+
+
 def home(request):
     return render(request,template_name='base.html')
+
 def new_search(request):
+    search = request.POST.get('search')
+    final_url = BASE_KRAIGSLIST_URL
+    response = requests.get(final_url)
+    data =  response.text
+    print(data)
+
     return render(request,template_name='Cam/new_search.html')
