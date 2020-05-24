48886b5ec097e3dd36d12ba35e7db2a385dbf69f
margaret mwangi <margaret13mwangi@gmail.com>
2020-05-24 21:47:03 +0300

    added git ignore

--- Cam/models.py
+++ Cam/models.py
@@ -5,10 +5,8 @@ class Search(models.Model):
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now=True)
 
-     def __str__(self):
-            return '{}'.format(self.search)
+    def __str__(self):
+        return '{}'.format(self.search)
 
-
-
-class Meta:
-    verbose_name_plural = 'Searches'
+    class Meta:
+        verbose_name_plural = 'Searches'
